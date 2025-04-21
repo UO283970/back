528e2d878044031907cd33bc902a93c9aa1d4259
UO283970 <98962779+UO283970@users.noreply.github.com>
2025-04-21 14:17:24 +0200

    Prueba de github secrets

--- src/main/java/tfg/books/back/model/userModels/PassDecryption.java
+++ src/main/java/tfg/books/back/model/userModels/PassDecryption.java
@@ -1,29 +1,42 @@
 package tfg.books.back.model.userModels;
 
+import com.google.api.client.util.Value;
+
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class PassDecryption {
-    private static final String SECRET_KEY = "1234567890123456";
+    @Value("${app.secret-key}")
+    private static final String SECRET_KEY = "";
     private static final String INIT_VECTOR = "RandomInitVector";
 
     public static String decrypt(String encrypted) {
-        try {
             IvParameterSpec ivParameterSpec = new IvParameterSpec(INIT_VECTOR.getBytes(StandardCharsets.UTF_8));
             SecretKeySpec secretKeySpec = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "AES");
 
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
 
             byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
             return new String(original);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException |
+                 InvalidAlgorithmParameterException | BadPaddingException | InvalidKeyException e) {
+            throw new RuntimeException(e);
         }
 
-        return null;
+
     }
 }
