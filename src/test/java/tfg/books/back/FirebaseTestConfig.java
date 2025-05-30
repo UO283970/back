5f7484d2fdf9fcbcf244340e522d521be515762d
UO283970 <98962779+UO283970@users.noreply.github.com>
2025-05-30 13:47:27 +0200

    Tests.

--- src/test/java/tfg/books/back/FirebaseTestConfig.java
+++ src/test/java/tfg/books/back/FirebaseTestConfig.java
@@ -1,9 +1,11 @@
 package tfg.books.back;
 
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import jakarta.annotation.PostConstruct;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.cloud.StorageClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -16,24 +18,35 @@ public class FirebaseTestConfig {
     @Value("${firebase.database.url}")
     private String databaseUrl;
 
-    @PostConstruct
-    public void initializeFirebase(){
+    @Bean
+    public FirebaseApp  initializeFirebase(){
         System.setProperty("FIREBASE_AUTH_EMULATOR_HOST", "localhost:9099");
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(new FakeGoogleCredentials())
-                .setDatabaseUrl(databaseUrl)
-                .setProjectId("project-id")
-                .build();
-
         if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(new FakeGoogleCredentials())
+                    .setDatabaseUrl(databaseUrl)
+                    .setProjectId("project-id")
+                    .build();
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
         }
     }
 
     @Bean
     public FirebaseAuth getFirebaseAuth(FirebaseApp app){
         return FirebaseAuth.getInstance(app);
+    }
+
+    @Bean
+    public Firestore firestore(FirebaseApp app) {
+        return FirestoreClient.getFirestore(app);
+    }
+
+    @Bean
+    public StorageClient storageClient(FirebaseApp app) {
+        return StorageClient.getInstance(app);
     }
 
 }
