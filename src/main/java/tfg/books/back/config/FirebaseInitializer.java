a4a1741540374ff1e900e9023662af8ffae50c67
UO283970 <98962779+UO283970@users.noreply.github.com>
2025-05-30 13:36:27 +0200

    Tests.

--- src/main/java/tfg/books/back/config/FirebaseInitializer.java
+++ src/main/java/tfg/books/back/config/FirebaseInitializer.java
@@ -10,12 +10,14 @@ import com.google.firebase.cloud.StorageClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+@Profile("!test")
 @Configuration
 @Service
 public class FirebaseInitializer {
