7f638f1c9fa251ac1931a51483b0dbe16a04033d
UO283970 <98962779+UO283970@users.noreply.github.com>
2025-06-30 11:03:26 +0200

    Nuevo despliegue

--- src/main/java/tfg/books/back/security/SecurityFilterChain.java
+++ src/main/java/tfg/books/back/security/SecurityFilterChain.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,7 +21,7 @@ public class SecurityFilterChain {
 @Bean
 public DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
     http
-      .csrf(csrf -> csrf.ignoringRequestMatchers("/graphql"))
+      .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(authManager -> authManager
         .requestMatchers("/graphql")
         .permitAll()
